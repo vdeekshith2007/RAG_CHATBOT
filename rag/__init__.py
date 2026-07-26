e958bde5dad1051dba3cc8b5f3840c53a3133ef2
vdeekshith2007 <vataparthideekshith18@gmail.com>
2026-07-26 08:45:59 +0530

    py is nmodified

--- rag/__init__.py
+++ rag/__init__.py
@@ -1,1 +1,11 @@
 # RAG Pipeline modules
+# RAG ARCHITECURE.
+#######       USING THIS LIBRARIES langchain
+ ######### langgraph
+####### llama-index
+ ##### chromadb
+#### ##### ####### sentence-transformers
+####  pymupdf
+##### pypdf
+####  ##### ######openai
+#  ### ### #### groq
